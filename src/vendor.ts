fba2546b59596f1c541f64d31db67e5e122bc96b
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-07 14:51:29 -0700

    added vendor

---
+++ src/vendor.ts
@@ -0,0 +1,17 @@
+// Angular 5
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Other vendors for example Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import "popper.js";
+import "jquery";
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "font-awesome-webpack2";
