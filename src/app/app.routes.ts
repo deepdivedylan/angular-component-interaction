533c38d0e12cc6374a7b71d3c8771f7ce0532c3b
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-07 14:55:03 -0700

    added app routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home.component";
+import {APP_BASE_HREF} from "@angular/common";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {DeepDiveInterceptor} from "./services/deep.dive.interceptor";
+
+
+export const allAppComponents = [HomeComponent];
+
+export const routes: Routes = [
+	{path: "", component: HomeComponent}
+];
+
+export const appRoutingProviders: any[] = [
+	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
+	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true},
+];
+
+export const routing = RouterModule.forRoot(routes);
