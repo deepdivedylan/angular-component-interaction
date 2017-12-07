0a80a2c86cb507231ba2e69edb2662f44531f692
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-07 14:56:56 -0700

    added app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector: 'angular-component-interaction-app',
+	templateUrl: './templates/angular-component-interaction-app.html'
+})
+
+export class AppComponent {}
