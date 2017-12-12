20bff7d6d99e7c0cd07759d85e55c82fcbc28e4e
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-12 11:40:22 -0700

    created reactive form

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,12 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 	templateUrl: "./templates/home.html"
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 	asleep: boolean = false;
 	fuzzySize: number = 256;
+	napLocationForm: FormGroup;
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.napLocationForm = this.formBuilder.group({
+			napLocation: ["", [Validators.required, Validators.maxLength(64)]]
+		});
+	}
 
 	getFuzzyUrl(): string {
 		let fuzzyBase = "https://placekitten.com/g/";
@@ -19,5 +29,9 @@ export class HomeComponent {
 
 	onFuzzySizeChange(newFuzzySize: number): void {
 		this.fuzzySize = newFuzzySize;
+	}
+
+	findNapLocation(): void {
+
 	}
 }
