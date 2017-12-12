1a0adf193b843f8a80e3372655079b0cfe049310
Dylan McDonald <dmcdonald21@cnm.edu>
2017-12-12 10:40:11 -0700

    made nap form reactive

--- src/app/components/nap.component.ts
+++ src/app/components/nap.component.ts
@@ -1,13 +1,23 @@
-import {Component, EventEmitter, Output} from "@angular/core";
+import {Component, EventEmitter, OnInit, Output} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
 	selector: "nap",
 	templateUrl: "./templates/nap.html"
 })
 
-export class NapComponent {
-
+export class NapComponent implements OnInit {
+	napForm: FormGroup;
+	napTime: number = 2000;
 	napStatus: string = "kitty is awake";
+
+	constructor(private formBuilder: FormBuilder) {}
+
+	ngOnInit(): void {
+		this.napForm = this.formBuilder.group({
+			napTime: [this.napTime, [Validators.required, Validators.min(0), Validators.max(10000)]]
+		});
+	}
 
 	sleep(ms: number): Promise<any> {
 		return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,5 +27,13 @@ export class NapComponent {
 		this.napStatus = "kitty is asleep";
 		this.sleep(ms);
 		this.napStatus = "kitty is awake";
+	}
+
+	setNapTime(newNapTime: number) : void {
+		this.napTime = newNapTime;
+	}
+
+	startNap() : void {
+
 	}
 }
